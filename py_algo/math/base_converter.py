88a23bc367ed0881ed50db7ef260e62497f96c76
Itrat Rahman <rahmanitrat@outlook.com>
2018-10-22 12:21:17 +0600

    constructed and tested the function divideBy2

--- py_algo/math/base_converter.py
+++ py_algo/math/base_converter.py
@@ -1,4 +1,4 @@
-from ..data_struct import stack
+from ..data_struct import Stack
 
 def divideBy2(number):
 
@@ -10,5 +10,17 @@ def divideBy2(number):
     Returns:
     binary_string - string containing binary of the number
     '''
+    # a stack to store the remainders
+    stack_of_remainders = Stack()
+
+    while number>0:
+        remainder = number % 2
+        stack_of_remainders.push(remainder)
+        number = number//2
+
+    binary_string = ""
+
+    while not stack_of_remainders.isEmpty():
+        binary_string = binary_string + str(stack_of_remainders.pop())
 
     return binary_string
--- py_algo/tests/test_base_converters.py
+++ py_algo/tests/test_base_converters.py
@@ -10,4 +10,10 @@ class TestBaseConverters(TestCase):
 
     def test_divide_by_2(self):
 
-        pass
+        self.assertTrue(divideBy2(42) == "101010")
+        self.assertTrue(divideBy2(300) == "100101100")
+        self.assertTrue(divideBy2(255) == "11111111")
+        self.assertTrue(divideBy2(53) == "110101")
+        self.assertTrue(divideBy2(1777) == "11011110001")
+        self.assertTrue(divideBy2(36) == "100100")
+        self.assertTrue(divideBy2(1000) == "1111101000")
