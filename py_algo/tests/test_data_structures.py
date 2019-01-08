f5583b2283d09ed68f1cb39972f13839fd79cec6
Itrat Rahman <rahmanitrat@outlook.com>
2019-01-08 19:03:01 +0600

    added test of stack to data structure test

--- py_algo/tests/test_data_structures.py
+++ py_algo/tests/test_data_structures.py
@@ -6,6 +6,8 @@ import random
 from py_algo.data_struct import Deque
 # import module to be tested
 from py_algo.data_struct import Queue
+# import module to be tested
+from py_algo.data_struct import Stack
 
 
 class TestDeque(TestCase):
@@ -113,3 +115,53 @@ class TestQueue(TestCase):
         self.assertTrue(s.size() == 3)
         s.enqueue("Apple")
         self.assertTrue(s.size() == 4)
+
+
+class TestStack(TestCase):
+    """
+    Test class to test the Stack module
+    """
+    def test_isEmpty(self):
+        """
+        A method to test the Stack isEmpty method
+        """
+        s = Stack()
+        self.assertTrue(s.isEmpty())
+
+    def test_peek(self):
+        """
+        A method to test the Stack peek mehtod
+        """
+        s = Stack()
+        s.push(4)
+        s.push('dog')
+        self.assertTrue(s.peek() == 'dog')
+
+    def test_pop(self):
+        """
+        A method to test the Stack pop method
+        """
+        s = Stack()
+        s.push(4)
+        s.push('dog')
+        s.push(True)
+        self.assertTrue(s.pop() == True)
+        self.assertTrue(s.pop() == 'dog')
+        self.assertTrue(s.pop() == 4)
+
+    def test_size(self):
+        """
+        A method to test the Stack size method
+        """
+        s = Stack()
+        s.push(4)
+        s.push('dog')
+        s.push(True)
+        self.assertTrue(s.size()==3)
+        s.pop()
+        self.assertTrue(s.size()==2)
+        s.pop()
+        self.assertTrue(s.size()==1)
+        s.pop()
+        self.assertTrue(s.size()==0)
+        self.assertTrue(s.isEmpty())
