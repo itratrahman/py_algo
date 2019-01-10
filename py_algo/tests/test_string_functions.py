a2e7ac5e7dae799f6813aab0f84f6e7b6d02186a
Itrat Rahman <rahmanitrat@outlook.com>
2019-01-10 13:04:38 +0600

    added TestParChecker to test of string functions

--- py_algo/tests/test_string_functions.py
+++ py_algo/tests/test_string_functions.py
@@ -4,6 +4,11 @@ from unittest import TestCase
 from py_algo.string import infix2postfix, compute_operation, postfixEval
 # import function to be tested
 from py_algo.string import pal_checker
+# import functions to be tested
+from py_algo.string import simple_par_checker
+from py_algo.string import match_symbol
+from py_algo.string import par_checker
+
 
 class TestInfixConverter(TestCase):
     """
@@ -35,6 +40,7 @@ class TestInfixConverter(TestCase):
         """
         self.assertTrue(postfixEval('7 8 + 3 2 + /') == 3.0 )
 
+
 class TestPalChecker(TestCase):
     """
     Test class to test the function palchecker
@@ -57,3 +63,54 @@ class TestPalChecker(TestCase):
         self.assertTrue(pal_checker("mallam"))
         self.assertTrue(pal_checker("ballab"))
         self.assertTrue(pal_checker("darad"))
+
+
+class TestParChecker(TestCase):
+    """
+    Test class to test the parenthesis checker functions
+    """
+    def test_simple_par_checker(self):
+        """
+        A method to check the function simple_par_checker
+        """
+        self.assertTrue(simple_par_checker('((()))'))
+        self.assertTrue(simple_par_checker('(((())))'))
+        self.assertTrue(simple_par_checker('(()()()())'))
+        self.assertTrue(simple_par_checker('(()((())()))'))
+        self.assertFalse(simple_par_checker('((())'))
+        self.assertFalse(simple_par_checker('(((()))'))
+        self.assertFalse(simple_par_checker('()))'))
+        self.assertFalse(simple_par_checker('(()()(()'))
+
+    def test_match_symbol(self):
+        """
+        A method to check the function match_symbol
+        """
+        self.assertTrue(match_symbol("{","}"))
+        self.assertTrue(match_symbol("(",")"))
+        self.assertTrue(match_symbol("[","]"))
+        self.assertFalse(match_symbol("{",")"))
+        self.assertFalse(match_symbol("{","]"))
+        self.assertFalse(match_symbol("(","]"))
+        self.assertFalse(match_symbol("(","}"))
+        self.assertFalse(match_symbol("[",")"))
+        self.assertFalse(match_symbol("[","}"))
+
+    def test_par_checker(self):
+        """
+        A method to check the function par_checker
+        """
+        self.assertTrue(par_checker('((()))'))
+        self.assertTrue(par_checker('(((())))'))
+        self.assertTrue(par_checker('(()()()())'))
+        self.assertTrue(par_checker('(()((())()))'))
+        self.assertFalse(par_checker('((())'))
+        self.assertFalse(par_checker('(((()))'))
+        self.assertFalse(par_checker('()))'))
+        self.assertFalse(par_checker('(()()(()'))
+        self.assertTrue(par_checker('{{([][])}()}'))
+        self.assertTrue(par_checker('[[{{(())}}]]'))
+        self.assertTrue(par_checker('[][][](){}'))
+        self.assertFalse(par_checker('([)]'))
+        self.assertFalse(par_checker('((()]))'))
+        self.assertFalse(par_checker('[{()]'))
