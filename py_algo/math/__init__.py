c2291303db7c8aeec2c2f0bac979b5e3ffb3feb1
Itrat Rahman <rahmanitrat@outlook.com>
2019-01-21 10:14:21 +0600

    transfered the sum of list function to seperate file

--- py_algo/math/__init__.py
+++ py_algo/math/__init__.py
@@ -1,6 +1,6 @@
 from .base_converters import divideBy2
 from .base_converters import base_converter
-from .math import sum_of_list
+from .sum_of_list import sum_of_list
 from .math import factorial
 from .math import power
 
