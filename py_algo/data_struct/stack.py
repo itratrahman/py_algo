087868b55ca8938f66590585dbb22cbe1ccee75e
Itrat Rahman <rahmanitrat@outlook.com>
2019-01-29 11:37:22 +0600

    Changed documentation of stack

--- py_algo/data_struct/stack.py
+++ py_algo/data_struct/stack.py
@@ -5,36 +5,39 @@ class Stack(object):
     """
     def __init__(self):
         """
-        class constructor which initialized the stack
+        Class constructor which initializes the stack
         """
         self.items = []
 
     def isEmpty(self):
         """
-        a method to check if the stack is empty
+        A method to check if the stack is empty
+        by comparing it to empty list
         """
         return self.items == []
 
     def push(self, item):
         """
-        a method to push item to the top of the stack
+        A method to push item to the top of the stack,
+        We use the list's append method
         """
         self.items.append(item)
 
     def pop(self):
         """
-        a method to pop item from the top of the stack
+        A method to pop item from top of the stack
+        We use the list's pop method
         """
         return self.items.pop()
 
     def peek(self):
         """
-        a method to return the top item of the stack
+        A method to return the top item of the stack
         """
         return self.items[len(self.items)-1]
 
     def size(self):
         """
-        a method to return the size of the stack
+        A method to return the size of the stack
         """
         return len(self.items)
