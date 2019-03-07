356d87994a8e9aaa81fb9080ead4c7997c733024
Itrat Rahman <rahmanitrat@outlook.com>
2019-03-07 13:02:28 +0600

    completed get and put methods of binary search tree

--- py_algo/tree/binary_search_tree.py
+++ py_algo/tree/binary_search_tree.py
@@ -87,3 +87,32 @@ class BinarySearchTree(object):
 
     def __setitem__(self,k,v):
         self.put(k,v)
+
+    def get(self,key):
+        if self.root:
+            res = self._get(key,self.root)
+            if res:
+                   return res.payload
+            else:
+                   return None
+        else:
+            return None
+
+    def _get(self,key,currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self._get(key,currentNode.leftChild)
+        else:
+            return self._get(key,currentNode.rightChild)
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __contains__(self,key):
+        if self._get(key,self.root):
+            return True
+        else:
+            return False
